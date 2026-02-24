a7544de5590f0924bd5e84c9b5b27e13f258e0fd
alakay13 <akhan101vc@gmail.com>
2026-02-24 14:33:03 -0500

    Added pom.xml file, added a Jenkinsfile

--- src/DiscountCalculator.java
+++ src/DiscountCalculator.java
@@ -1,4 +1,10 @@
 public class DiscountCalculator {
+    public static void main(String[] args) {
+        double price = 200.0;
+        int percentage = 50;
+
+        System.out.println("An item with a price of " + price + "with a " + percentage + "% discount will be $" + calculateDiscount(price, percentage));
+    }
 
     public static double calculateDiscount(double price, int percentage) {
         if (price <= 0 || percentage < 0) {
